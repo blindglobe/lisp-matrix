cca2d8ce8937437e529e8b452d0858803c84f634
Evan Monroig <evan.monroig@gmail.com>
2008-05-20 18:13:58 +0900

    * experimental/element-access.lisp (with-fast-access): try at 	using a higher-level macro that defines local macros to override 	VREF and gain 10x speed.

--- experimental/element-access.lisp
+++ experimental/element-access.lisp
@@ -1,4 +1,4 @@
-;;; Time-stamp: <2008-05-19 17:21:16 Evan Monroig>
+;;; Time-stamp: <2008-05-20 18:12:38 Evan Monroig>
 ;;; 
 ;;; Here I made several experiments to see how to improve vector and
 ;;; matrix element access.
@@ -8,6 +8,7 @@
 (in-package :lisp-matrix)
 
 (import 'cl-utilities:once-only)
+(import 'cl-utilities:with-unique-names)
 
 (defconstant +asize+ 4000000)
 (declaim (type fixnum +asize+))
@@ -176,3 +177,106 @@
 (define-compiler-macro vref (&whole form x i)
   (declare (ignore x i))
   form)
+
+
+;;; 4) higher-level macro
+;;;
+;;; The idea is to have a macro `with-fast-access' that inlines the
+;;; calls to VREF and MREF for appropriate matrix types.  There are a
+;;; few possibilities to do this: (i) evaluate the code once we know
+;;; the matrix type, i.e. at runtime; (ii) include type declarations
+;;; in the macro.
+;;;
+;;; With (ii), a call to the macro might look like this
+
+#+nil
+(let ((a (make-vector 10 'double :initial-element 1d0))
+      (s 0d0))
+  (with-fast-access ((a vector-double))
+    (dotimes (i 10)
+      (incf s (vref a i))))
+  s)
+
+;;; which would be expanded to
+
+#+nil
+(let ((a (make-vector 10 'double :initial-element 1d0))
+      (s 0d0))
+  (let ((DATA1 (data a)))
+    (dotimes (i 10)
+      (declare (type fixnum i))
+      (incf s (the double-float (fnv-double-ref DATA1 i))))
+    s))
+
+;;; Here is a try at this using LABELS and MACROLET to rebind the
+;;; function VREF.  This won't work for (SETF VREF), and macros
+;;; WITH-FAST-ACCESS can't be nested.
+;;;
+;;; Note: this looks similar to what Cyrrus Harmon did in his CLEM
+;;; package, who solved the problem of not working with SETF by
+;;; expanding in all cases (instead of falling back to the previous
+;;; VREF), and nesting by having a symbol-macrolet store the variables
+;;; for which to enable fast access.
+;;;
+;;; A full-fledged version would use a code walker but that's another
+;;; can of worms..
+
+(defmethod vref-call ((type (eql 'vector-double)) data-var i-var)
+  ``(the double-float (fnv-double-ref ,',data-var ,,i-var)))
+
+(defmacro with-fast-access ((&rest forms) &body body)
+  "Inline all calls to VREF or AREF in BODY for each vector or matrix
+  in FORMS.
+
+  This works by first binding a function to the old VREF function, and
+  then defining a local macro VREF which expands into optimized code
+  for each matrix in FORMS, and falls back to the function otherwise."
+  (with-unique-names (a i vref)
+    (loop for (var type) in forms
+       for g = (gensym "DATA")
+       collect `(,g (data ,var)) into data-bindings
+       collect `(,var ,(vref-call type g i)) into vref-cases
+       finally
+       (return `(let (,@data-bindings)
+                  (labels ((,vref (,a ,i) (vref ,a ,i)))
+                    (declare (inline ,vref))
+                    (macrolet ((vref (,a ,i)
+                                 (case ,a
+                                   ,@vref-cases
+                                   (t `(,',vref ,,a ,,i)))))
+                      ,@body)))))))
+
+(defun lisp-matrix-fast-vref-benchmark ()
+  "Here we use the macro WITH-FAST-ACCESS which inlines calls to
+  VREF."
+  (let ((a (make-vector +asize+ 'double :initial-element 1d0))
+        (s 0d0))
+    (declare (type double-float s)
+             (type vector-double a))
+    (with-fast-access ((a vector-double))
+      (dotimes (i +asize+ s)
+        (declare (type fixnum i))
+        (incf s (vref a i))))))
+
+#+nil
+(time (lisp-matrix-fast-vref-benchmark))
+;; => 0.074 s
+
+(defun lisp-matrix-fast-vref-benchmark2 ()
+  "same as LISP-MATRIX-FAST-VREF-BENCHMARK but we `forgot' to add the
+  matrix in the list of matrices to optimize fast access for and
+  thereby fall back to the rebound version of VREF (macro-expand the
+  macro WITH-FAST-ACCESS in this function and in
+  LISP-MATRIX-FAST-VREF-BENCHMARK to see the difference)."
+  (let ((a (make-vector +asize+ 'double :initial-element 1d0))
+        (s 0d0))
+    (declare (type double-float s)
+             (type vector-double a))
+    (with-fast-access ()
+      (dotimes (i +asize+ s)
+        (declare (type fixnum i))
+        (incf s (vref a i))))))
+
+#+nil
+(time (lisp-matrix-fast-vref-benchmark2))
+;; => 0.723 s
