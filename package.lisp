694a0e237decb7702c986f0f26cbcf39dea840f5
AJ Rossini <blindglobe@gmail.com>
2008-09-17 09:32:02 +0200

    set up package for external use, and also add a user experimentation package.

--- package.lisp
+++ package.lisp
@@ -1,14 +1,41 @@
 ;;;; lisp-matrix package definition.
 ;;;; Author: mfh
 
+(in-package :cl-user)
+
 (defpackage :lisp-matrix
   (:use :cl
-        :cl-utilities
         :cffi
+        :cl-utilities
         :org.middleangle.foreign-numeric-vector
-	:fiveam
-        :cl-blapack)
-  
-  (:import-from :fnv))
+        :cl-blapack
+	:ffa
+	:fiveam)
+  (:import-from :fnv)
+  (:export make-matrix make-matrix*  ;; basic instantiations
+	   strides-class window-class transpose-class
+	   strides window transpose 
+	   ones zeros eye rand ;; types 
+	   copy copy! copy*
+	   copy-maybe copy-maybe*
+	   fill-matrix
 
-(in-package :lisp-matrix)
+	   m= m* m+ m- 
+	   v= v* v+ v- 
+
+	   print-object
+	   mref data row col
+	   nelts  nrows ncols
+	   matrix-dimension matrix-dimensions
+	   orientation valid-orientation-p opposite-orientation
+	   flatten-matrix-indices flatten-matrix-indices-1
+
+	   la-simple-matrix-double  la-simple-matrix-integer
+	   la-simple-matrix-complex  la-simple-matrix-float ;; guessed! wrong?
+	   
+	   la-simple-vector-double la-simple-vector-integer
+	   ))
+
+(defpackage :lisp-matrix-user
+  (:use :cl
+	:lisp-matrix))
