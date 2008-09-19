8d05f06f15ff92660d940c4493581c6b5c508999
AJ Rossini <blindglobe@gmail.com>
2008-09-19 18:25:10 +0200

    more exports for the packaging, but some mght be wrong.

--- package.lisp
+++ package.lisp
@@ -8,7 +8,7 @@
         :cffi
         :cl-utilities
         :org.middleangle.foreign-numeric-vector
-        :cl-blapack
+        :org.middleangle.cl-blapack
 	:ffa
 	:fiveam)
   (:import-from :fnv)
@@ -31,9 +31,18 @@
 	   flatten-matrix-indices flatten-matrix-indices-1
 
 	   la-simple-matrix-double  la-simple-matrix-integer
-	   la-simple-matrix-complex  la-simple-matrix-float ;; guessed! wrong?
+	   ;; Next 3 symbols are guesses at... wrong?
+	   la-simple-matrix-complex  la-simple-matrix-float 
+	   la-simple-matrix-fixnum 
 	   
-	   la-simple-vector-double la-simple-vector-integer
+	   la-simple-vector-double
+	   la-simple-vector-integer
+
+	   ;; Next paragrah of symbols are guesses... wrong?
+	   fa-simple-matrix-double  fa-simple-matrix-integer
+	   fa-simple-matrix-complex  fa-simple-matrix-float 
+	   fa-simple-vector-double fa-simple-vector-integer
+	   fa-simple-matrix-fixnum 
 	   ))
 
 (defpackage :lisp-matrix-user
