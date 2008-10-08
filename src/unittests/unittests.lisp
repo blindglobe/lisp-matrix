a63503a1d539464af1e79f9224993d19b0f3a13e
AJ Rossini <blindglobe@gmail.com>
2008-10-08 19:44:08 +0200

    add test for testing transposition of matrices and views.

--- src/unittests/unittests.lisp
+++ src/unittests/unittests.lisp
@@ -306,7 +306,6 @@ are discarded \(that is, the body is an implicit PROGN)."
     (ensure-error (mref m1 2 5))))
 
 
-
 (addtest (lisp-matrix-ut)
  make-matrix-double-zero-size
  #-clisp (for-all-implementations
@@ -488,6 +487,21 @@ are discarded \(that is, the body is an implicit PROGN)."
       (ensure (eql (class-name (class-of (strides a :nrows 2)))
                (window-class a)))
       (ensure (eq a (parent (strides (strides a :ncols 2 :col-stride 2))))))))
+
+
+(addtest (lisp-matrix-ut-matrix-views)
+  indexing-views
+  (let* ((m3 (make-matrix 6 5 :initial-contents '((1d0 2d0 3d0 4d0 5d0)
+						  (6d0  7d0  8d0  9d0  10d0)
+						  (11d0 12d0 13d0 14d0 15d0)
+						  (16d0 17d0 18d0 19d0 20d0)
+						  (21d0 22d0 23d0 24d0 25d0)
+						  (26d0 27d0 28d0 29d0 30d0))))
+	 (m4 (strides m3 :nrows 2 :row-stride 2)))
+    (ensure (v= (row m4 1)
+		(col (transpose m4) 1) ))
+    (ensure (v= (col m3 1) (row (transpose m3) 1)) )
+    (ensure (v= (row m3 1) (col (transpose m3) 1)))))
 
 ;;; VECTORS
 
