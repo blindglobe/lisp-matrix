acb40cf28b01f397aceb2ecaad4c2b7432d58f56
AJ Rossini <blindglobe@gmail.com>
2008-12-07 18:08:01 +0100

    copy of vector-like/vector/list -> vector-like (we had the other way around, sigh).

--- src/vector.lisp
+++ src/vector.lisp
@@ -521,3 +521,33 @@
     (dotimes (i (nelts a))
       (setf (nth i b) (vref a i)))) ;; aref for vectors, but for lists? 
   b)
+
+(defmethod copy! ((a list) (b vector-like))
+  (unless (eq a b) ;; don't worry about same objects
+    ;; FIXME: care about fast copy once everything is working
+    (assert (= (nelts a) (length b)))
+    ;; FIXME: is the following possible?
+    (assert (subtypep (element-type a) (element-type b)))
+    (dotimes (i (nelts a))
+      (setf (vref b i) (nth i a)))) ;; aref for vectors, but for lists? 
+  b)
+
+(defmethod copy! ((a vector) (b vector-like))
+  (unless (eq a b) ;; don't worry about same objects
+    ;; FIXME: care about fast copy once everything is working
+    (assert (= (nelts a) (length b)))
+    ;; FIXME: is the following possible?
+    (assert (subtypep (element-type a) (element-type b)))
+    (dotimes (i (nelts a))
+      (setf (vref b i) (aref a i)))) ;; aref for vectors, but for lists? 
+  b)
+
+(defmethod copy! ((a vector-like) (b vector-like))
+  (unless (eq a b) ;; don't worry about same objects
+    ;; FIXME: care about fast copy once everything is working
+    (assert (= (nelts a) (length b)))
+    ;; FIXME: is the following possible?
+    (assert (subtypep (element-type a) (element-type b)))
+    (dotimes (i (nelts a))
+      (setf (vref b i) (vref a i)))) ;; aref for vectors, but for lists? 
+  b)
