9181d32d768bf798bbf69b5092028487a1b5a7c6
AJ Rossini <blindglobe@gmail.com>
2009-01-03 20:04:20 +0100

    data conversion tools needed, issue added plus some code.

--- src/data-transform.lisp
+++ src/data-transform.lisp
@@ -1,6 +1,6 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2008-12-16 17:28:35 tony>
+;;; Time-stamp: <2009-01-03 20:00:06 tony>
 ;;; Creation:   <2008-12-02 17:28:08 tony>
 ;;; File:       data-transform.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
@@ -21,16 +21,20 @@
 ;;; to convert between lists and vectors, and between lists of lists
 ;;; and matrices.  This is the point of the functions here.
 
+;;; While any sensible lisp programmer would savor the API, I´m
+;;; corrupting it in favor of getting-stuff-done.
+
 (in-package :lisp-matrix)
 
-(defun list->vector-like (listvar)
+(defun list->vector-like (listvar &key (coerce-to 'double-float))
   "Create a vector-like using default implementation. Use globals to
 change implementation, etc."
   (make-vector (length listvar)
 	       :type :row
 	       :initial-contents
 	       (list 
-		(mapcar #'(lambda (x) (coerce x 'double-float))
+		;; (mapcar #'(lambda (x) (coerce x 'double-float))
+		(mapcar #'(lambda (x) (coerce x coerce-to))
 			listvar))))
 
 
@@ -41,10 +45,22 @@ change implementation, etc."
       (setf (aref result i) (vref vecvar i)))
     result))
 
+#| coding logic only at this point.
+(defun listoflists->matrix-like (lol &optional (coerce-to 'double-float))
+  (let ((dims (loop lol counting number of subunits, first number is
+		 number in inside, second is nil if they don't all
+		 equal each other))
+	)
+    (let ((result (apply #'make-matrix dims))
+	  )
+      (loop-over-lol counting i j and setting value, and
+	   (setf (mref result i j) value)))  )  )
+|#
+
+
 #|
 (defun matrix-like->listoflists ())
 
-(defun listoflists->matrix-like ())
 
 
 (defun matrix-like->array ())
