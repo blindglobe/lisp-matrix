1664ec73f8a263cdca7447f06c8a116a9fb859d2
AJ Rossini <blindglobe@gmail.com>
2009-06-09 08:49:15 +0200

    factorization API and datastructures for seamless use of factorization structures.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- src/numerical-linear-algebra.lisp
+++ src/numerical-linear-algebra.lisp
@@ -1,6 +1,6 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2009-02-21 16:24:42 tony>
+;;; Time-stamp: <2009-06-09 08:47:59 tony>
 ;;; Creation:   <2009-02-05 11:18:51 tony>
 ;;; File:       numerical.linear.algebra.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
@@ -16,31 +16,76 @@
 
 ;;; Matrix Factorization for stable processing
 
-(defgeneric factor (a &key type)
+;; current factorization types, only worried about "high-level"
+;; descriptions, not further ramifications (size/spce considerations).
+
+;; we have: SVD, LU, QR, Cholesky.
+;; note that some are "special case only" factorizations.
+
+(defclass matrix-factorized-results ()
+  ((results
+    :initarg :results
+    :reader results)
+   (factorization-type
+    :initarg :type
+    :reader :factorization-type)))
+
+(defgeneric factorized-matrix (a)
+  (:documentation "Return the matrix (and not the structure) which is
+  the result from factorization routine")
+  (:method ((a matrix-factorized-results))
+    (ecase (factorization-type a)
+      (:qr )
+      (:lu )
+      (:cholesky)
+      (:svd)))
+  (:method ((a matrix-like))
+    (warn "Returning same matrix, assuming prior factorization.")))
+
+(defgeneric factorize (a &key by)
   (:documentation "matrix decomposition, M -> SVD/LU/AtA etc")
-  (:method ((a matrix-like) &key (type :qr))
-    (let (results  (ecase type
-		     (:qr (geqrf a) )
-		     (:lu ( a))
-		     (:cholesky (potrf a))
-		     (:svd (gesvf a))) ))
-    results))
+  (:method ((a data-frame-like) &key by)
+    (factorize (data-frame-like->matrix-like a) :by by))
+  (:method ((a matrix-like) &key (by :qr)) ;; is this the right way to get :qr as default?
+    (make-instance 'matrix-factorized-results
+		   :results (ecase by
+			      (:qr (geqrf a))
+			      (:lu (getrf a))
+			      (:cholesky (potrf a))
+			      (:svd (gesvf a)))
+		   :type by)))
+
 
 
 ;;; inversion based on factorization
 
-(defgeneric invert (a)
+(defgeneric invert (a &optional by)
   (:documentation "compute inverse of A using the appropriate factorization.")
-  (:method ((a factored-matrix))
-    (let (results  (ecase (factor-type a)
+  (:method ((a factorized-matrix-results) &optional by)
+    (unless (equal by (factorization-by a))
+      (warn "method to factor BY does not match FACTORIZATION-TYPE."))
+    (let ((results (ecase (factorization-type a)
 		     (:qr (geqri a) )
 		     (:lu ( a))
 		     (:cholesky (potri a))
 		     (:svd (gesvi a))
-		     (:otherwise (error
-				  "No LAPACK/BLAS method for inversion of class ~S"
-				  (factor-type a))))))
-    results))
+		     (:otherwise
+		      (error
+		       "Unimplemented or not a proper factorized-matrix type."
+		       (factor-type a))))))
+      results))
+  (:method ((a matrix-like) &optional by)
+    (if (not by) (setf by :qr))
+    (let ((results (ecase by
+		     (:qr (geqri a) )
+		     (:lu ( a))
+		     (:cholesky (potri a))
+		     (:svd (gesvi a))
+		     (:otherwise
+		      (error
+		       "Unimplemented or not a proper factorized-matrix type."
+		       (factor-type a))))))
+      results)))
 
 
 ;;; [W|G]LS solutions
