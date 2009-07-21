385bf1f10590fe870bfeca82aa3bdc477362a3f3
AJ Rossini <blindglobe@gmail.com>
2009-07-21 08:20:20 +0200

    cleaning up misc data-trans functions, docs, etc.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- src/data-transform.lisp
+++ src/data-transform.lisp
@@ -1,6 +1,6 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2009-04-16 17:34:21 tony>
+;;; Time-stamp: <2009-07-20 18:27:26 tony>
 ;;; Creation:   <2008-12-02 17:28:08 tony>
 ;;; File:       data-transform.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
@@ -19,16 +19,18 @@
 
 ;;; Somewhere, we need to include some helper functions which allow us
 ;;; to convert between lists and vectors, and between lists of lists
-;;; and matrices.  This is the point of the functions here.
+;;; and matrices.  This is the only point of this file's functions.
 
 ;;; While any sensible lisp programmer would savor the API, I´m
 ;;; corrupting it in favor of getting-stuff-done.
 
 (in-package :lisp-matrix)
 
+;;; LIST <-> VECTOR-LIKE
+
 (defun list->vector-like (listvar &key
 			  (coerce-to 'double-float)
-			  (orientation :column))
+			  (orientation :row))
   "Create a vector-like using default implementation. Use globals to
 change implementation, etc.  By default, we assume lists are
 variables, not cases (i.e. follow lispstat convention), and therefore
@@ -52,8 +54,31 @@ convert to column."
       (setf (aref result i) (vref vecvar i)))
     result))
 
-#| coding logic only at this point.
- (defun listoflists->matrix-like (lol &optional (coerce-to 'double-float))
+;;; LISTOFLIST <-> MATRIX-LIKE
+
+
+(defun listsoflists-consistent-dimensions-p (&rest list-of-list-names)
+  "Check if the lengths of the lists are equal (T, otherwise NIL), to
+justify further processing and initial conditions."
+  (if (< 0  (reduce #'(lambda (x y) (if  (= x y) y -1))
+		    (mapcar #'length list-of-list-names)))
+      T nil))
+
+
+(defun listoflists-dimensions (lol)
+  "We assume row-major, that is each sublist denotes a row, and columns are formed by taking the jth element from each list to form the jth column"
+  (list (length lol)
+	(length (map 'list (lambda (x) 1) lol))))
+
+(defparameter *lol-1*  (list (list 1 2 3 4)
+			     (list 5 6 7 8)))
+(map 'list #'list *lol-1*)
+(map 'list (lambda (x) x) *lol-1*)
+(listoflists-dimensions *lol-1*)
+
+
+
+(defun listoflists->matrix-like (lol &optional (coerce-to 'double-float))
   (let ((dims (loop lol counting number of subunits, first number is
 		 number in inside, second is nil if they don't all
 		 equal each other))
@@ -63,16 +88,10 @@ convert to column."
       (loop-over-lol counting i j and setting value, and
 	   (setf (mref result i j) value)))  )  )
 
-
  (defun matrix-like->listoflists ())
-
-
-
  (defun matrix-like->array ())
-
  (defun array->matrix-like ())
 |#
-
 
 
 ;; FIXME: this function needs to be:
@@ -86,6 +105,7 @@ convert to column."
 triagular storage into full storage).  For non-square matrices, there
 might be a bit of excess to ignore; but we only handle the top square
 of the rectangle."
+  (check-type m matrix-like)
   (let ((mindim (reduce #'min (matrix-dimensions m)))
 	(result (copy m)))
     (ecase type
@@ -98,26 +118,11 @@ of the rectangle."
     result))
 
 #|
-
-  (defun trap2mat (m &key (type :upper))
-    "convert a upper/lower triangular storage to an actual normal but
-  symmetric matrix.  
-  FIXME: Current only workis for square matrices -- needs to work for
-  the square-ish minimal sized square matrix within a rectangular
-  matrix."
-    (check-type m matrix-like)
-    (let ((mc (copy m)))
-      (dotimes (i (nrows m))
-	(dotimes (j i)
-	  (ecase type
-	    (:upper (setf (mref mc i j) (mref m j i)))
-	    (:lower (setf (mref mc j i) (mref m i j))))))
-      mc))
-
  (defparameter *trap2mat-test1*
   (make-matrix 3 3 :initial-contents '((1d0 2d0 3d0) (4d0 5d0 6d0) (7d0 8d0 9d0))))
  (assert (matrix-like-symmetric-p (trap2mat *trap2mat-test1*)))
  (assert (matrix-like-symmetric-p (trap2mat *trap2mat-test1* :type :upper)))
  (assert (matrix-like-symmetric-p (trap2mat *trap2mat-test1* :type :lower)))
-
 |#
+
+;;; ListOfList functions
