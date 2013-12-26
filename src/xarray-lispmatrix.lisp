3667d8dc467f5f98d9f52ff6b7cfc8bf8ecab7ab
AJ Rossini <blindglobe@gmail.com>
2013-12-26 17:26:59 +0100

    use matrix-like virtual class as a requirement.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- src/xarray-lispmatrix.lisp
+++ src/xarray-lispmatrix.lisp
@@ -1,6 +1,6 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2009-09-19 23:27:09 tony>
+;;; Time-stamp: <2013-12-26 13:13:00 tony>
 ;;; Creation:   <2009-06-26 07:57:23 tony>
 ;;; File:       xarray-lispmatrix.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
@@ -28,25 +28,36 @@
 |#
 
 (defmethod xtype ((object matrix-like))
-  (warning "Not implemented for matrix-like"))
+  (warning "Not implemented for matrix-like virtual class"))
 
-(defmethod xrank ((object matrix-like)))
-(defmethod xdims ((object matrix-like)))
+(defmethod xrank ((object matrix-like))
+  (warning "Not implemented for matrix-like virtual class"))
+
+
+(defmethod xdims ((object matrix-like))
+  (warning "Not implemented for matrix-like virtual class"))
 ;; (defmethod xdims*) can just use the default method.
-(defmethod xdim ((object matrix-like)))
-(defmethod xsize ((object matrix-like)))
+(defmethod xdim ((object matrix-like))
+  (warning "Not implemented for matrix-like virtual class"))
+(defmethod xsize ((object matrix-like))
+  (warning "Not implemented for matrix-like virtual class"))
 (defmethod xref-writable-p ((object matrix-like) &rest subscripts)
   "Always true for matrix-like derived classes at this point (right?)"
   t)
-(defmethod xref ((object matrix-like) &rest subscripts))
+(defmethod xref ((object matrix-like) &rest subscripts)
+  (mref object subscripts))
+
 (defmethod (setf xref) (value (object matrix-like) &rest subscripts))
+
+
 ;; there is a default method that should suffice, but perhaps some
-  ;; direct tricks could make this more efficient?  Worth a look
-  ;; LATER. 
-  (defmethod xsetf ((destination matrix-like)
-		    (source matrix-like)
-		    &key map-function))
-  ;; (defmethod take) ; default should work for now.
+;; direct tricks could make this more efficient?  Worth a look
+;; LATER. 
+(defmethod xsetf ((destination matrix-like)
+		  (source matrix-like)
+		  &key map-function))
+
+;; (defmethod take) ; default should work for now.
 
 
 
